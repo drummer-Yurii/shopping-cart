ca675d4c4a64f38a8fa3840aec94b7c54a93488e
Yurii Rybachok <drummer2009@ukr.net>
2023-11-21 11:17:50 +0200

    create header component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="container">
+    <TheHeader />
     <router-view></router-view>
   </div>
 </template>
 
 <script setup>
-
+import TheHeader from './components/TheHeader.vue';
 </script>
 
 <style scoped>
